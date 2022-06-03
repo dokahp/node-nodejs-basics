df99f4f3ccecd9dbfcfcd3a8b4d6a86400fb7166
Vitaliy Dreko <vitali.dreko@gmail.com>
2022-06-03 15:56:20 +0300

    feat: add cli/args solution

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,7 @@
 export const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2);
+    const result = args.map((el, i) => i % 2? `${el},`: `${el} is`);
+    console.log(result.join(' ').slice(0, -1));
 };
+
+parseArgs();
